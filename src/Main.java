e747a3ed6605fbb611787f91ae29b133ea591a6b
Cullen <cw32801@gmail.com>
2024-08-04 12:12:59 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@
  */
 public class Automobile {
     // Private variables to store the automobile information
-    private String make;
-    private String model;
-    private String color;
-    private int year;
+    private final String make;
+    private final String model;
+    private final String color;
+    private final int year;
 
     /**
      * Constructor to initialize an Automobile object.
@@ -23,42 +23,21 @@ public class Automobile {
         this.year = year;
     }
 
-    /**
-     * Adds vehicle information.
-     *
-     * @param make  The make of the automobile.
-     * @param model The model of the automobile.
-     * @param color The color of the automobile.
-     * @param year  The year of the automobile.
-     * @return A message indicating the vehicle has been added.
-     */
-    public String addVehicle(String make, String model, String color, int year) {
-        this.make = make;
-        this.model = model;
-        this.color = color;
-        this.year = year;
-        return "Vehicle added: " + make + " " + model + " " + color + " " + year;
+    // Getters for accessing the automobile attributes
+    public String getMake() {
+        return make;
     }
 
-    /**
-     * Removes vehicle information if the provided details match the stored values.
-     *
-     * @param make  The make of the automobile.
-     * @param model The model of the automobile.
-     * @param color The color of the automobile.
-     * @param year  The year of the automobile.
-     * @return A message indicating the result of the removal attempt.
-     */
-    public String removeVehicle(String make, String model, String color, int year) {
-        if (this.make.equals(make) && this.model.equals(model) && this.color.equals(color) && this.year == year) {
-            this.make = null;
-            this.model = null;
-            this.color = null;
-            this.year = 0;
-            return "Vehicle removed: " + make + " " + model + " " + color + " " + year;
-        } else {
-            return "Mismatch: Vehicle not found.";
-        }
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
     }
 
     /**
@@ -66,19 +45,14 @@ public class Automobile {
      *
      * @return A string representation of the vehicle information.
      */
-    public String displayVehicle() {
-        if (make != null) {
-            return "Vehicle: " + make + " " + model + " " + color + " " + year;
-        } else {
-            return "No vehicle information available.";
-        }
+    @Override
+    public String toString() {
+        return "Vehicle: " + make + " " + model + " " + color + " " + year;
     }
 
     public static void main(String[] args) {
-        // Example 
+        // Example usage of the Automobile class
         Automobile car = new Automobile("Honda", "Fit", "Red", 2010);
-        System.out.println(car.displayVehicle());
-        System.out.println(car.removeVehicle("Honda", "Fit", "Red", 2010));
-        System.out.println(car.displayVehicle());
+        System.out.println(car);
     }
 }
